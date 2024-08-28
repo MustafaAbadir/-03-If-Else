d590538e4000b8c0356d421169c8ef163235e65b
MustafaAbadir <Mustafa_As@hotmail.com>
2024-08-28 20:04:11 +0200

    Uppgift 1 (Even or Odd)

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Write a number!");
+int myNumber = int.Parse(Console.ReadLine());
+
+if (myNumber%2 == 0)
+{
+    Console.WriteLine("The number is even");
+}
+else
+{
+    Console.WriteLine("The number is odd");
+}
