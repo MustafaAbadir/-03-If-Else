b34a46e2322d53332e865fc546e3de1b65a97225
MustafaAbadir <Mustafa_As@hotmail.com>
2024-08-28 20:35:45 +0200

    Uppgift 2 (Age Bracket)

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*Even or Odd*/
 Console.WriteLine("Write a number!");
-int myNumber = int.Parse(Console.ReadLine());
+int myNumber = int.Parse(Console.ReadLine()); 
 
 if (myNumber%2 == 0)
 {
@@ -9,4 +10,20 @@ if (myNumber%2 == 0)
 else
 {
     Console.WriteLine("The number is odd");
+}
+
+/*Age Bracket*/
+Console.WriteLine("How old are you?");
+int age = int.Parse(Console.ReadLine());
+if (age < 13)
+{
+    Console.WriteLine("You are a child");
+}
+else if (age > 13 && age < 19)
+{
+    Console.WriteLine("You are a teenager");
+}
+else
+{
+    Console.WriteLine("You are an adult");
 }
