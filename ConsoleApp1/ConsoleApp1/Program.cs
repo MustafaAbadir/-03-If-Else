959999790af4e3099a2ed07a9252b52c516f478d
MustafaAbadir <Mustafa_As@hotmail.com>
2024-08-28 21:38:15 +0200

    Uppgift 3 - test score

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 /*Even or Odd*/
 Console.WriteLine("Write a number!");
-int myNumber = int.Parse(Console.ReadLine()); 
+int myNumber = int.Parse(Console.ReadLine());
 
-if (myNumber%2 == 0)
+if (myNumber % 2 == 0)
 {
     Console.WriteLine("The number is even");
 }
@@ -11,6 +11,8 @@ else
 {
     Console.WriteLine("The number is odd");
 }
+
+
 
 /*Age Bracket*/
 Console.WriteLine("How old are you?");
@@ -26,4 +28,29 @@ else if (age > 13 && age < 19)
 else
 {
     Console.WriteLine("You are an adult");
+}
+
+/*Ask the user to input their test score*/
+
+Console.WriteLine("Write your test score 0-100");
+int score= int.Parse(Console.ReadLine());
+if (score >= 90 && score <= 100)
+{
+    Console.WriteLine("A");
+}
+else if (score >= 80 && score <= 89)
+{
+    Console.WriteLine("B");
+}
+else if (score >= 70 && score <= 79)
+{
+    Console.WriteLine("C");
+}
+else if (score >= 60 && score <= 69)
+{
+    Console.WriteLine("D");
+}
+else
+{
+    Console.WriteLine("F");
 }
